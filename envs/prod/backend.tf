fc7b4cbcdb41818e00351d159c1916dedb9b6832
marciomfilho <107187625+marciomfilho@users.noreply.github.com>
2026-02-04 14:35:15 -0300

    remove backend

--- envs/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tracknow-tf-state-lab"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tracknow-tf-lock-lab"
-    encrypt        = true
-  }
-}
