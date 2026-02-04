588abecda91fd1006ffb247b6a1edcf2277df0c3
marciomfilho <107187625+marciomfilho@users.noreply.github.com>
2026-02-04 14:32:04 -0300

    fix region

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tracknow-tf-state-lab"
     key            = "prod/terraform.tfstate"
-    region         = "ua-east-1"
+    region         = "us-east-1"
     dynamodb_table = "tracknow-tf-lock-lab"
     encrypt        = true
   }
