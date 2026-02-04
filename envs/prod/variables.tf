6cdbdfa7f354ade7a656855c6347c94a78fec796
marciomfilho <107187625+marciomfilho@users.noreply.github.com>
2026-02-04 10:59:48 -0300

    variables

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -1,3 +1,13 @@
+variable "lab_role_arn" {
+  type    = string
+  default = "arn:aws:iam::167582247690:role/LabRole"
+}
+
+variable "region" {
+  type    = string
+  default = "sa-east-1"
+}
+
 variable "db_master_password" {
   type      = string
   sensitive = true
